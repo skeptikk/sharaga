b0fbf07ca29af9d0f415150488ecafc0006c76ec
skeptikk <71062820+skeptikk@users.noreply.github.com>
2020-09-14 10:52:52 +0300

    Add files via upload

---
+++ N1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << "Enter a number: "; // просим пользователя ввести любое число
+	int a = 0;
+	std::cin >> a; // получаем пользовательское число и сохраняем его в переменную a
+	std::cout << "You entered " << a << std::endl;
+	return 0;
+}
